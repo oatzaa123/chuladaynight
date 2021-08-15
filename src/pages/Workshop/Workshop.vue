d495a08cbf0c7cbc5832e675281bcbc074932e9e
srawit <srawit2540@gmail.com>
2021-08-15 23:40:19 +0700

    workshop

--- src/pages/Workshop/Workshop.vue
+++ src/pages/Workshop/Workshop.vue
@@ -1,143 +1,148 @@
 <template>
-    <div class="workshop">
-        <div class="workshop-title">
-            <h1>
-                WORK SHOP
-                <p class="n">Art</p>
-            </h1>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div
-                    class="workshop-content col-6 col-md-4"
-                    v-for="item in image"
-                    :key="item"
-                    @click.stop="onHandleClick(item.id)"
-                >
-                    <img :src="item.image" />
-                </div>
-            </div>
-        </div>
+  <div class="workshop">
+    <div v-for="i in 2" :key="i" class="star-group">
+      <div class="blur"></div>
+      <div class="star"></div>
     </div>
+    <div class="workshop-title">
+      <h1>
+        WORK SHOP
+        <p class="n">Art</p>
+      </h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div
+          class="workshop-content col-6 col-md-4"
+          v-for="item in image"
+          :key="item"
+          @click.stop="onHandleClick(item.id)"
+        >
+          <img :src="item.image" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, inject, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, inject, onMounted } from "vue";
+import { useRouter } from "vue-router";
 export default {
-    setup() {
-        const image = ref([
-            {
-                id: 1,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 2,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 3,
-                image: require('@/assets/images/workshop/Image 50@4X.png'),
-            },
-            {
-                id: 4,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 5,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 6,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 7,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 8,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 9,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 10,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 11,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-            {
-                id: 12,
-                image: require('@/assets/images/workshop/image-workshop.png'),
-            },
-        ])
+  setup() {
+    const image = ref([
+      {
+        id: 1,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 2,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 3,
+        image: require("@/assets/images/workshop/Image 50@4X.png"),
+      },
+      {
+        id: 4,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 5,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 6,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 7,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 8,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 9,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 10,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 11,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+      {
+        id: 12,
+        image: require("@/assets/images/workshop/image-workshop.png"),
+      },
+    ]);
 
-        const router = useRouter()
-        const globalStore = inject('globalStore')
+    const router = useRouter();
+    const globalStore = inject("globalStore");
 
-        const setBackgroundImage = () => {
-            globalStore.changeBackground(
-                require('@/assets/images/BACKGROUND@2X.png')
-            )
-        }
+    const setBackgroundImage = () => {
+      globalStore.changeBackground(
+        require("@/assets/images/BACKGROUND@2X.png")
+      );
+    };
 
-        onMounted(() => {
-            setBackgroundImage()
-        })
+    onMounted(() => {
+      setBackgroundImage();
+    });
 
-        const onHandleClick = (id) => {
-            router.push({ name: 'Workshop-id', params: { id } })
-        }
+    const onHandleClick = (id) => {
+      router.push({ name: "Workshop-id", params: { id } });
+    };
 
-        return { image, onHandleClick }
-    },
-}
+    return { image, onHandleClick };
+  },
+};
 </script>
 
 <style lang="scss">
-@import '@/assets/css/container.scss';
+@import "@/assets/css/container.scss";
+@import "@/assets/css/workshop.scss";
 .workshop {
-    .workshop-title {
-        padding-bottom: 2rem;
-        h1 {
-            position: relative;
-            font-size: 58px;
-            font-weight: 600;
-            color: #ffffff;
-            text-align: center;
-            letter-spacing: 0px;
-            text-shadow: 1px 1px 1px white, 0 0 5px white, 0 0 5px white;
-        }
-        .n {
-            position: absolute;
-            font-family: 'Savoye Regular';
-            font-weight: 100;
-            color: #b61bff;
-            left: 55.8%;
-            font-size: 60px;
-            top: 35%;
-            text-shadow: none;
-        }
+  .workshop-title {
+    padding-bottom: 2rem;
+    h1 {
+      position: relative;
+      font-size: 58px;
+      font-weight: 600;
+      color: #ffffff;
+      text-align: center;
+      letter-spacing: 0px;
+      text-shadow: 1px 1px 1px white, 0 0 5px white, 0 0 5px white;
     }
-    .container {
-        padding-top: var(--bs-gutter-x, 0);
-        .workshop-content {
-            margin: 5px -5px;
-            img {
-                width: 100%;
-                height: 100%;
-                image-rendering: auto;
-                image-rendering: pixelated;
-            }
-            img:hover {
-                cursor: pointer;
-            }
-        }
+    .n {
+      position: absolute;
+      font-family: "Savoye Regular";
+      font-weight: 100;
+      color: #b61bff;
+      left: 55.8%;
+      font-size: 60px;
+      top: 35%;
+      text-shadow: none;
     }
+  }
+  .container {
+    padding-top: var(--bs-gutter-x, 0);
+    .workshop-content {
+      margin: 5px -5px;
+      img {
+        width: 100%;
+        height: 100%;
+        image-rendering: auto;
+        image-rendering: pixelated;
+      }
+      img:hover {
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
