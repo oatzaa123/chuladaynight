7afca890cf0b12f27795b8cd89da8abf198b8a9d
Pa OaTz InDy <oatkung2541@gmail.com>
2021-08-17 23:38:05 +0700

    update section 3

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
     <div>
         <Section1></Section1>
         <Section2></Section2>
+        <Section3></Section3>
         <Section4></Section4>
         <Section5></Section5>
         <Section6></Section6>
@@ -11,6 +12,7 @@
 <script>
 import Section1 from '../components/home/section1.vue'
 import Section2 from '../components/home/section2.vue'
+import Section3 from '../components/home/section3.vue'
 import Section4 from '../components/home/section4.vue'
 import Section5 from '../components/home/section5.vue'
 import Section6 from '../components/home/section6.vue'
@@ -18,6 +20,7 @@ export default {
     components: {
         Section1,
         Section2,
+        Section3,
         Section4,
         Section5,
         Section6,
