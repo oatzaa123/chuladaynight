7211568b5111dac921c341516e526f009d9df98d
srawit <srawit2540@gmail.com>
2021-08-17 15:53:23 +0700

    section2

--- src/components/home/section2.vue
+++ src/components/home/section2.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="section2" :style="section2Height">
-    <!-- <div v-for="i in 5" :key="i" class="star-group">
-      <div class="blur"></div>
-      <div class="star"></div>
-    </div> -->
     <div class="container">
       <div class="title">
         โครงการ “Chula Art Town”
@@ -12,70 +8,75 @@
         ซึ่งให้การสนับสนุนอุปกรณ์สีสำหรับสร้างสรรค์ผลงาน เนื่องในวาระครบรอบ 36
         ปี ของคณะศิลปกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย
       </div>
+    </div>
 
-      <div class="content" :style="section2Height">
+    <div class="content" :style="section2Height">
+      <div v-for="i in 5" :key="i" class="star-group">
+        <div class="blur"></div>
+        <div class="star"></div>
+      </div>
+      <img
+        :src="require('@/assets/images/home/section2/Group 654.svg')"
+        class="map"
+      />
+      <div class="left-top">
+        <p>
+          โครงการ “Chula Art Town”
+          เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
+        </p>
         <img
-          :src="require('@/assets/images/home/section2/Group 654.svg')"
-          class="map"
+          :src="require('@/assets/images/home/section2/Group 812.png')"
+          class="logo"
         />
-        <div class="left-top">
-          <p>
-            โครงการ “Chula Art Town”
-            เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 812.png')"
-            class="logo"
-          />
-          <img
-            :src="require('@/assets/images/home/section2/Path 61.svg')"
-            class="path"
-          />
-        </div>
-        <div class="right-top">
-          <p>
-            โครงการ “Chula Art Town”
-            เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 809.png')"
-            class="logo"
-          />
-          <img
-            :src="require('@/assets/images/home/section2/Path 50.svg')"
-            class="path"
-          />
-        </div>
-        <div class="left-bottom">
-          <p>
-            โครงการ “Chula Art Town”
-            เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 811.png')"
-            class="logo"
-          />
-          <img
-            :src="require('@/assets/images/home/section2/Path 124.svg')"
-            class="path"
-          />
-        </div>
-        <div class="right-bottom">
-          <p>
-            โครงการ “Chula Art Town”
-            เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 810.png')"
-            class="logo"
-          />
-          <img
-            :src="require('@/assets/images/home/section2/Path 420.svg')"
-            class="path"
-          />
-        </div>
+        <img
+          :src="require('@/assets/images/home/section2/Path 61.svg')"
+          class="path"
+        />
+      </div>
+      <div class="right-top">
+        <p>
+          โครงการ “Chula Art Town”
+          เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
+        </p>
+        <img
+          :src="require('@/assets/images/home/section2/Group 809.png')"
+          class="logo"
+        />
+        <img
+          :src="require('@/assets/images/home/section2/Path 50.svg')"
+          class="path"
+        />
+      </div>
+      <div class="left-bottom">
+        <p>
+          โครงการ “Chula Art Town”
+          เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
+        </p>
+        <img
+          :src="require('@/assets/images/home/section2/Group 811.png')"
+          class="logo"
+        />
+        <img
+          :src="require('@/assets/images/home/section2/Path 124.svg')"
+          class="path"
+        />
+      </div>
+      <div class="right-bottom">
+        <p>
+          โครงการ “Chula Art Town”
+          เป็นความร่วมมือกันระหว่างสำนักงานจัดการทรัพย์สิน
+        </p>
+        <img
+          :src="require('@/assets/images/home/section2/Group 810.png')"
+          class="logo"
+        />
+        <img
+          :src="require('@/assets/images/home/section2/Path 420.svg')"
+          class="path"
+        />
       </div>
     </div>
+    <!-- </div> -->
   </div>
 </template>
 
