644c9d0fb36efdb81574c3e57a23e93fb9d3f2ac
srawit <srawit2540@gmail.com>
2021-08-17 23:10:09 +0700

    responsive section5

--- src/components/home/section5.vue
+++ src/components/home/section5.vue
@@ -1,25 +1,21 @@
 <template>
-    <div class="section5">
-        <News :limit="3" parent="section5" />
-    </div>
+  <div class="section5">
+    <News :limit="3" parent="section5" />
+  </div>
 </template>
 
 <script>
-import News from '@/pages/New/News'
+import News from "@/pages/New/News";
 export default {
-    components: {
-        News,
-    },
-    setup() {
-        return {}
-    },
-}
+  components: {
+    News,
+  },
+  setup() {
+    return {};
+  },
+};
 </script>
 
-<style>
-.section5 {
-    background-color: #030124;
-    min-height: 100vh;
-    padding: 3rem;
-}
+<style lang="scss">
+@import "../../assets/css/home/section5.scss";
 </style>
