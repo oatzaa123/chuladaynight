2b331088e97c44971431760e481f589f4f53104e
Pa OaTz InDy <oatkung2541@gmail.com>
2021-08-20 14:31:25 +0700

    update css gallery

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -1,63 +1,63 @@
 <template>
-  <div class="right">
-    <router-link
-      :to="{ path: item.path }"
-      v-for="item in Menu"
-      :key="item.key"
-      class="menu-link"
-    >
-      <div>{{ item.name }}</div>
-    </router-link>
-    <div class="menu-lang">
-      <span class="th">TH</span>&nbsp;|&nbsp;<span class="en">EN</span>
+    <div class="right">
+        <router-link
+            :to="{ path: item.path }"
+            v-for="item in Menu"
+            :key="item.key"
+            class="menu-link"
+        >
+            <div>{{ item.name }}</div>
+        </router-link>
+        <div class="menu-lang">
+            <span class="th">TH</span>&nbsp;|&nbsp;<span class="en">EN</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { computed } from "vue";
-import { useStore } from "vuex";
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 export default {
-  setup() {
-    const store = useStore();
+    setup() {
+        const store = useStore()
 
-    return {
-      Menu: computed(() => store.getters["showMenuList"]),
-    };
-  },
-};
+        return {
+            Menu: computed(() => store.getters['showMenuList']),
+        }
+    },
+}
 </script>
 
 <style lang="scss" scoped>
 .right {
-  width: 35%;
-  line-height: 1.55;
-  font-family: "Kanit-Medium";
-  font-size: 17px;
-  margin-top: 15px;
-  opacity: 0;
-  height: 0px;
-  overflow: hidden;
-  transition: opacity 1s, height 1s;
-  &.show {
-    opacity: 1;
-    height: auto;
-    overflow: unset;
-  }
-  .menu-link {
-    color: #707070;
-    text-decoration: unset;
-    &.router-link-active {
-      color: #ffffff;
+    width: 35%;
+    line-height: 1.55;
+    font-family: 'Kanit-Medium';
+    font-size: 17px;
+    margin-top: 15px;
+    opacity: 0;
+    height: 0px;
+    overflow: hidden;
+    transition: opacity 1s, height 1s;
+    &.show {
+        opacity: 1;
+        height: auto;
+        overflow: unset;
     }
-  }
-  .menu-lang {
-    display: flex;
-    color: #707070;
-    text-decoration: unset;
-    span:nth-child(1) {
-      margin-right: 1px;
+    .menu-link {
+        color: #707070;
+        text-decoration: unset;
+        &.router-link-active {
+            color: #ffffff;
+        }
     }
-  }
+    .menu-lang {
+        display: flex;
+        color: #707070;
+        text-decoration: unset;
+        span:nth-child(1) {
+            margin-right: 1px;
+        }
+    }
 }
 </style>
