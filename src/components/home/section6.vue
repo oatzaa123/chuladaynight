70336d65d6e0ae78d3e19e51fb6d838ee95ac494
srawit <srawit2540@gmail.com>
2021-08-23 19:36:26 +0700

    .

--- src/components/home/section6.vue
+++ src/components/home/section6.vue
@@ -27,9 +27,11 @@ import { Splide, SplideSlide } from "@splidejs/vue-splide";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 import { onMounted, reactive, toRefs } from "vue";
 
-import * as Three from "three";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import Stats from "three/examples/jsm/libs/stats.module";
+
 export default {
   components: {
     Splide,
@@ -74,49 +76,75 @@ export default {
           },
         },
       },
-      camera: null,
       scene: null,
+      light: null,
+      camera: null,
       renderer: null,
-      loader: null,
-      loader2: null,
+      controls: null,
+      objLoader: null,
+      stats: null,
     });
 
     const init = () => {
-      console.log("init");
       let container = document.getElementById("section6container");
-      //camera
-      state.camera = new Three.PerspectiveCamera(
-        70,
+
+      state.scene = new THREE.Scene();
+      state.scene.add(new THREE.AxesHelper(5));
+
+      state.light = new THREE.PointLight();
+      state.light.position.set(2.5, 7.5, 15);
+      state.scene.add(state.light);
+
+      state.camera = new THREE.PerspectiveCamera(
+        75,
         container.clientWidth / container.clientHeight,
-        0.01,
-        10
+        0.1,
+        1000
       );
-      state.camera.position.z = 1;
-      //scene
-      state.scene = new Three.Scene();
-      //obj loader
-      state.loader = new OBJLoader();
-      state.loader.load("../../assets/3d/male02.obj", (loadedObject) => {
-        state.scene.add(loadedObject);
-        console.log("obj", loadedObject);
-      });
+      state.camera.position.z = 3;
 
-      state.loader2 = new MTLLoader();
-      state.loader2.load("../../assets/3d/male02.mtl", (loadedObject) => {
-        state.scene.add(loadedObject);
-        console.log("mtl", loadedObject);
-      });
-
-      //renderer
-      state.renderer = new Three.WebGLRenderer({ antialias: true });
-      state.renderer.setSize(container.clientWidth, container.clientHeight);
-      state.renderer.setClearColor("#ff0");
+      state.renderer = new THREE.WebGLRenderer();
+      state.renderer.setSize(window.innerWidth, window.innerHeight);
       container.appendChild(state.renderer.domElement);
+
+      state.controls = new OrbitControls(
+        state.camera,
+        state.renderer.domElement
+      );
+      state.controls.enableDamping = true;
+
+      state.objLoader = new OBJLoader();
+      state.objLoader.load(
+        "../../assets/3d/male02.obj",
+        (object) => {
+          // (object.children[0] as THREE.Mesh).material = material
+          // object.traverse(function (child) {
+          //     if ((child as THREE.Mesh).isMesh) {
+          //         (child as THREE.Mesh).material = material
+          //     }
+          // })
+          state.scene.add(object);
+        },
+        (xhr) => {
+          console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+      state.stats = Stats();
+      container.appendChild(state.stats.dom);
     };
 
     const animate = () => {
-      console.log("animate");
       requestAnimationFrame(animate);
+      state.controls.update();
+      render();
+      state.stats.update();
+    };
+
+    const render = () => {
       state.renderer.render(state.scene, state.camera);
     };
 
