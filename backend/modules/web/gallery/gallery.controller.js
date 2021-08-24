ef44901eba88b9eb30d64c78290465d32d9ce7a8
srawit <srawit2540@gmail.com>
2021-08-24 18:15:40 +0700

    ..

--- backend/modules/web/gallery/gallery.controller.js
+++ backend/modules/web/gallery/gallery.controller.js
@@ -1,195 +1,206 @@
-const catchAsync = require('../../../helpers/catchAsync')
-const APIFeatures = require('../../../utils/apiFeatures')
-const ErrorHandler = require('../../../helpers/errorHandler')
-const Gallery = require('./../../models/gallery.model')
-const { uploadFile, uploadVideo } = require('./../../../middleware/upload')
+const catchAsync = require("../../../helpers/catchAsync");
+const APIFeatures = require("../../../utils/apiFeatures");
+const ErrorHandler = require("../../../helpers/errorHandler");
+const Gallery = require("./../../models/gallery.model");
+const { uploadFile, uploadVideo } = require("./../../../middleware/upload");
 
 exports.getGalleries = catchAsync(async (req, res, next) => {
-    const featuresGallery = new APIFeatures(Gallery.find(), req.query)
-        .filter()
-        .limitFields()
-        .paginate()
-        .sort()
+  const featuresGallery = new APIFeatures(Gallery.find(), req.query)
+    .filter()
+    .limitFields()
+    .paginate()
+    .sort();
 
-    const AllGallery = await featuresGallery.query
+  const AllGallery = await featuresGallery.query;
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            total: AllGallery.length,
-            Gallery: AllGallery,
-        },
-    })
-})
+  res.status(200).json({
+    status: "success",
+    data: {
+      total: AllGallery.length,
+      Gallery: AllGallery,
+    },
+  });
+});
 
 exports.getGallery = catchAsync(async (req, res, next) => {
-    const { id } = req.params
-    const gallery = await Gallery.findById(id)
+  const { id } = req.params;
+  const gallery = await Gallery.findById(id);
 
-    if (!gallery) return next(new ErrorHandler('Data not found', 404))
+  if (!gallery) return next(new ErrorHandler("Data not found", 404));
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            gallery,
-        },
-    })
-})
+  res.status(200).json({
+    status: "success",
+    data: {
+      gallery,
+    },
+  });
+});
 
 exports.nextGallery = catchAsync(async (req, res, next) => {
-    const { id } = req.params
-    const AllGallery = await Gallery.find()
-    const gallery = AllGallery.find((item) => item._id.toString() === id)
+  const { id } = req.params;
+  const AllGallery = await Gallery.find();
+  const gallery = AllGallery.find((item) => item._id.toString() === id);
 
-    if (!gallery) return next(new ErrorHandler('Data not found', 404))
+  if (!gallery) return next(new ErrorHandler("Data not found", 404));
 
-    const nextId = await Gallery.findOne({
-        createdAt: { $gt: gallery.createdAt },
-    })
+  const nextId = await Gallery.findOne({
+    createdAt: { $gt: gallery.createdAt },
+  });
 
-    const nextGallery = nextId
-        ? nextId
-        : AllGallery.find((_, index) => index === 0)
+  const nextGallery = nextId
+    ? nextId
+    : AllGallery.find((_, index) => index === 0);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            gallery: nextGallery,
-        },
-    })
-})
+  res.status(200).json({
+    status: "success",
+    data: {
+      gallery: nextGallery,
+    },
+  });
+});
 
 exports.perviousGallery = catchAsync(async (req, res, next) => {
-    const { id } = req.params
-    const AllGallery = await Gallery.find()
-    const gallery = AllGallery.find((item) => item._id.toString() === id)
+  const { id } = req.params;
+  const AllGallery = await Gallery.find();
+  const gallery = AllGallery.find((item) => item._id.toString() === id);
 
-    if (!gallery) return next(new ErrorHandler('Data not found', 404))
+  if (!gallery) return next(new ErrorHandler("Data not found", 404));
 
-    // const perviousId = await Gallery.findOne({
-    //     createdAt: { $lt: gallery.createdAt },
-    // })
+  // const perviousId = await Gallery.findOne({
+  //     createdAt: { $lt: gallery.createdAt },
+  // })
 
-    const perviousId = AllGallery.findIndex((item) => item._id === gallery._id)
+  const perviousId = AllGallery.findIndex((item) => item._id === gallery._id);
 
-    const perviousGallery =
-        perviousId > 0
-            ? AllGallery.find((_, index) => index === perviousId - 1)
-            : AllGallery.find((_, index) => index === AllGallery.length - 1)
+  const perviousGallery =
+    perviousId > 0
+      ? AllGallery.find((_, index) => index === perviousId - 1)
+      : AllGallery.find((_, index) => index === AllGallery.length - 1);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            gallery: perviousGallery,
-        },
-    })
-})
+  res.status(200).json({
+    status: "success",
+    data: {
+      gallery: perviousGallery,
+    },
+  });
+});
 
-exports.addGallery = catchAsync(async (req, res, next) => {
-    let coverImageName, authorImageName, gallery
-    const contents = []
+exports.addGallery = catchAsync(async (req, res) => {
+  const { title, author, path, shortDescription, location, description } =
+    req.body;
 
-    const { author, path, description } = JSON.parse(req.body.data)
+  var contentImages = [];
+  var contentVideos = [];
+  var coverImageName;
+  var authorImageName;
+  if (req.files) {
+    const { coverImage, authorImage, contentVideo, contentImage } = req.files;
 
-    if (req.files) {
-        const { coverImage, authorImage, video, contentImage } = req.files
-        if (coverImage || authorImage || contentImage) {
-            if (contentImage.length > 0) {
-                await Promise.all(
-                    contentImage.map(async (item) => {
-                        contents.push({
-                            path,
-                            contentType: 'Image',
-                            contentValue: await uploadFile(item, path),
-                        })
-                    })
-                )
-            } else if (contentImage) {
-                contents.push({
-                    path,
-                    contentType: 'Image',
-                    contentValue: await uploadFile(contentImage, path),
-                })
-            }
-
-            if (coverImage) {
-                coverImageName = {
-                    path,
-                    name: await uploadFile(coverImage, path),
-                }
-            }
-
-            if (authorImage) {
-                authorImageName = {
-                    path,
-                    name: await uploadFile(authorImage, path),
-                }
-            }
-        }
-
-        if (video) {
-            if (video.length > 0) {
-                await Promise.all(
-                    video.map(async (item, index) => {
-                        contents.push({
-                            path,
-                            contentType: 'Video',
-                            contentValue: await uploadVideo(item, path),
-                        })
-                    })
-                )
-            } else {
-                contents.push({
-                    path,
-                    contentType: 'Video',
-                    contentValue: await uploadVideo(video, path),
-                })
-            }
-        }
-
-        let obj = {
-            ...JSON.parse(req.body.data),
-            coverImage: coverImageName,
-            author: {
-                ...author,
-                image: authorImageName,
-            },
-        }
-
-        gallery = new Gallery({
-            ...obj,
-            content: contents,
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-        })
-
-        if (typeof description !== 'string') {
-            description.map((item) => {
-                gallery.content.push({
-                    contentType: 'Text',
-                    contentValue: item,
-                })
-            })
-        } else {
-            gallery.content.push({
-                contentType: 'Text',
-                contentValue: description,
-            })
-        }
-
-        await gallery.save()
-    } else {
-        gallery = await Gallery.create({
-            ...JSON.parse(req.body.data),
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-        })
+    if (contentImage) {
+      if (contentImage.length > 0) {
+        contentImages = contentImage;
+      } else {
+        contentImages.push(contentImage);
+      }
     }
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            gallery,
-        },
-    })
-})
+    if (contentVideo) {
+      if (contentVideo.length > 0) {
+        contentVideos = contentVideo;
+      } else {
+        contentVideos.push(contentVideo);
+      }
+    }
+
+    if (coverImage) {
+      coverImageName = {
+        path,
+        name: await uploadFile(coverImage, path),
+      };
+    }
+
+    if (authorImage) {
+      authorImageName = {
+        path,
+        name: await uploadFile(authorImage, path),
+      };
+    }
+  } // req.file
+
+  var arrImg = [];
+  if (contentImages.length > 0) {
+    await Promise.all(
+      contentImages.map(async (item) => {
+        arrImg.push({
+          path,
+          contentType: "Image",
+          contentValue: await uploadFile(item, path),
+          contentName: item.name,
+        });
+      })
+    );
+  }
+
+  var arrVdo = [];
+  if (contentVideos.length > 0) {
+    await Promise.all(
+      contentVideos.map(async (item) => {
+        arrVdo.push({
+          path,
+          contentType: "Video",
+          contentValue: await uploadVideo(item, path),
+          contentName: item.name,
+        });
+      })
+    );
+  }
+
+  var contents = [];
+  await description.map(async (item) => {
+    var img = arrImg.filter((i) => {
+      return i.contentName === item;
+    });
+
+    var vdo = arrVdo.filter((i) => {
+      return i.contentName === item;
+    });
+
+    if (img.length > 0) {
+      contents.push({
+        path: img[0].path,
+        contentType: img[0].contentType,
+        contentValue: img[0].contentValue,
+      });
+    } else if (vdo.length) {
+      contents.push({
+        path: vdo[0].path,
+        contentType: vdo[0].contentType,
+        contentValue: vdo[0].contentValue,
+      });
+    } else {
+      contents.push({
+        contentType: "Text",
+        contentValue: item,
+      });
+    }
+  });
+
+  var gallery = new Gallery({
+    title,
+    author: { ...JSON.parse(author), image: authorImageName },
+    coverImage: coverImageName,
+    path,
+    shortDescription,
+    location: JSON.parse(location),
+    content: contents,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  });
+
+  await gallery.save();
+
+  res.status(201).json({
+    status: "success",
+    data: gallery,
+  });
+});
--- src/components/home/section6.vue
+++ src/components/home/section6.vue
@@ -1,161 +1,83 @@
 <template>
-    <div class="section6">
-        <!-- <div class="container"> -->
-        <div class="header">PARNTER</div>
-        <div class="slide">
-            <splide :options="slideOptions">
-                <splide-slide
-                    v-for="slide in slides"
-                    :key="slide.src"
-                    class="slide-items"
-                >
-                    <div class="img">
-                        <img :src="slide.src" />
-                    </div>
-                    <div class="name">Partner name</div>
-                </splide-slide>
-            </splide>
-        </div>
-        <!-- </div> -->
-
-        <div id="section6container" style="height: 500px; width: 500px"></div>
+  <div class="section6">
+    <!-- <div class="container"> -->
+    <div class="header">PARNTER</div>
+    <div class="slide">
+      <splide :options="slideOptions">
+        <splide-slide
+          v-for="slide in slides"
+          :key="slide.src"
+          class="slide-items"
+        >
+          <div class="img">
+            <img :src="slide.src" />
+          </div>
+          <div class="name">Partner name</div>
+        </splide-slide>
+      </splide>
     </div>
+    <!-- </div> -->
+  </div>
 </template>
 
 <script>
-import { Splide, SplideSlide } from '@splidejs/vue-splide'
-import '@splidejs/splide/dist/css/themes/splide-default.min.css'
-import { onMounted, reactive, toRefs } from 'vue'
-
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+import { reactive, toRefs } from "vue";
 
 export default {
-    components: {
-        Splide,
-        SplideSlide,
-    },
-    setup() {
-        const state = reactive({
-            slides: [
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-                {
-                    src: require('../../assets/images/home/section1/L_DN2N_20202.png'),
-                },
-            ],
-            slideOptions: {
-                type: 'loop',
-                perPage: 5,
-                rewind: true,
-                pagination: false,
-                breakpoints: {
-                    768: {
-                        perPage: 3,
-                    },
-                },
-            },
-        })
+  components: {
+    Splide,
+    SplideSlide,
+  },
+  setup() {
+    const state = reactive({
+      slides: [
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+        {
+          src: require("../../assets/images/home/section1/L_DN2N_20202.png"),
+        },
+      ],
+      slideOptions: {
+        type: "loop",
+        perPage: 5,
+        rewind: true,
+        pagination: false,
+        breakpoints: {
+          768: {
+            perPage: 3,
+          },
+        },
+      },
+    });
 
-        const scene = new THREE.Scene()
-        scene.add(new THREE.AxesHelper(5))
-
-        const light = new THREE.PointLight()
-        light.position.set(2.5, 7.5, 15)
-        scene.add(light)
-
-        const camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
-        )
-        camera.position.z = 100
-
-        const renderer = new THREE.WebGLRenderer()
-        renderer.setClearColor('Gainsboro')
-        renderer.setSize(500, 500)
-
-        const controls = new OrbitControls(camera, renderer.domElement)
-        controls.enableDamping = true
-
-        const objLoader = new OBJLoader()
-        // const file = 'static/models/obj/windmill.obj'
-        const file =
-            'http://localhost:5000/modelImage/Way of life and faith/All.obj'
-        objLoader.load(
-            file,
-            (object) => {
-                // (object.children[0] as THREE.Mesh).material = material
-                // object.traverse(function (child) {
-                //     if ((child as THREE.Mesh).isMesh) {
-                //         (child as THREE.Mesh).material = material
-                //     }
-                // })
-                console.log('obj', object)
-                scene.add(object)
-            },
-            (xhr) => {
-                console.log('xhr', xhr)
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log(error)
-            }
-        )
-
-        window.addEventListener('resize', onWindowResize, false)
-        function onWindowResize() {
-            camera.aspect = 500 / 500
-            camera.updateProjectionMatrix()
-            renderer.setSize(500, 500)
-            render()
-        }
-
-        const animate = () => {
-            requestAnimationFrame(animate)
-            controls.update()
-            render()
-        }
-
-        const render = () => {
-            renderer.render(scene, camera)
-        }
-
-        onMounted(() => {
-            let container = document.getElementById('section6container')
-            container.appendChild(renderer.domElement)
-
-            animate()
-        })
-
-        return {
-            ...toRefs(state),
-        }
-    },
-}
+    return {
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../../assets/css/home/section6.scss';
+@import "../../assets/css/home/section6.scss";
 </style>
