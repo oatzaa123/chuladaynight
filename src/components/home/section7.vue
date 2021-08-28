e74ece19b0b9887c7461ae02ff2cb30b7b392d87
srawit <srawit2540@gmail.com>
2021-08-28 16:02:41 +0700

    section7

---
+++ src/components/home/section7.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="section-7">
+    <div class="img">
+      <img :src="require('@/assets/images/home/section7/Group 795.svg')" />
+    </div>
+    <div class="text">
+      <div>Tell us what</div>
+      <div>you think</div>
+    </div>
+    <div class="button">
+      <button class="btn" @click="link">แสดงความคิดเห็น</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {
+    const link = () => {
+      window.open("https://www.google.com");
+    };
+
+    return {
+      link,
+    };
+  },
+};
+</script>
+<style lang="scss">
+.section-7 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 70px;
+  background-color: #030124;
+  color: #fff;
+  .img {
+    margin-right: 25px;
+  }
+  .text {
+    font-size: 50px;
+    line-height: 1;
+    font-family: "Kanit-SemiBold";
+    margin-right: 70px;
+  }
+  .button .btn {
+    color: #fff;
+    font-family: "Prompt-Bold";
+    font-size: 22px;
+    background-color: #7948e6;
+    padding: 15px 20px;
+    border-radius: 32px;
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,6 +6,7 @@
     <Section4></Section4>
     <Section5></Section5>
     <Section6></Section6>
+    <Section7></Section7>
   </div>
 </template>
 
@@ -16,6 +17,7 @@ import Section3 from "../components/home/section3.vue";
 import Section4 from "../components/home/section4.vue";
 import Section5 from "../components/home/section5.vue";
 import Section6 from "../components/home/section6.vue";
+import Section7 from "../components/home/section7.vue";
 export default {
   components: {
     Section1,
@@ -24,6 +26,7 @@ export default {
     Section4,
     Section5,
     Section6,
+    Section7,
   },
 };
 </script>
