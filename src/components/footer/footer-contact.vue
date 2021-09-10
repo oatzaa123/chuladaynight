05ae7c38f50492d301bb7434769f9c336fe97cf2
srawit <srawit2540@gmail.com>
2021-09-10 23:30:23 +0700

    .

--- src/components/footer/footer-contact.vue
+++ src/components/footer/footer-contact.vue
@@ -1,159 +1,118 @@
 <template>
-    <div class="contact">
-        <div class="contact-group">
-            <div class="box-1">
-                <div>{{ $t('footer_company_name') }}</div>
-                <div>
-                    {{ $t('footer_company_address') }}
-                </div>
-                <div>{{ $t('footer_company_address2') }}</div>
-            </div>
-            <div class="box-2">
-                <div class="icon">
-                    <img
-                        :src="
-                            require('@/assets/images/home/footer/Image 58.png')
-                        "
-                        alt=""
-                    />
-                </div>
-                <div>
-                    <div>{{ $t('footer_description4') }}</div>
-                    <div style="color: #1ba7ff">Google art & Culture</div>
-                </div>
-            </div>
-            <div class="box-3">
-                <div v-for="(item, index) in icon" :key="index">
-                    <img
-                        @click="openUrl(item.url, item.name)"
-                        class="contact-icon"
-                        :src="item.path"
-                        width="40"
-                    />
-                </div>
-            </div>
+  <div class="contact">
+    <div class="contact-group">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 address">
+          <div>{{ $t("footer_company_name") }}</div>
+          <div>
+            {{ $t("footer_company_address") }}
+          </div>
+          <div>{{ $t("footer_company_address2") }}</div>
+          <div>{{ $t("footer_company_address3") }}</div>
+          <div>{{ $t("footer_company_address4") }}</div>
         </div>
-        <div class="contact-group">
-            <div class="box-1">
-                <div>{{ $t('footer_company_address3') }}</div>
-                <div>{{ $t('footer_company_address4') }}</div>
-            </div>
-            <div class="box-2"></div>
-            <div class="box-3"></div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 google-culture">
+          <div class="icon">
+            <img
+              :src="require('@/assets/images/home/footer/Image 58.png')"
+              alt=""
+            />
+          </div>
+          <div>
+            <div>{{ $t("footer_description4") }}</div>
+            <div style="color: #1ba7ff">Google art & Culture</div>
+          </div>
         </div>
+        <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12 icon-social">
+          <template v-for="(item, index) in icon" :key="index">
+            <img
+              @click="openUrl(item.url, item.name)"
+              class="contact-icon"
+              :src="item.path"
+              width="40"
+            />
+          </template>
+        </div>
+      </div>
     </div>
-    <div class="spaceline"></div>
+  </div>
+  <div class="spaceline"></div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-    setup() {
-        const icon = ref([
-            {
-                path: require('@/assets/images/icons/Group 327@2x.png'),
-                url: 'https://www.facebook.com',
-                name: 'GridWhiz',
-            },
-            {
-                path: require('@/assets/images/icons/Group 344@2x.png'),
-                url: 'https://www.instagram.com',
-                name: 'gridwhiz',
-            },
-            {
-                path: require('@/assets/images/icons/Group 328.svg'),
-            },
-        ])
+  setup() {
+    const icon = ref([
+      {
+        path: require("@/assets/images/icons/Group 327@2x.png"),
+        url: "https://www.facebook.com",
+        name: "GridWhiz",
+      },
+      {
+        path: require("@/assets/images/icons/Group 344@2x.png"),
+        url: "https://www.instagram.com",
+        name: "gridwhiz",
+      },
+      {
+        path: require("@/assets/images/icons/Group 328.svg"),
+      },
+    ]);
 
-        const openUrl = (url, name) => {
-            window.open(`${url}/${name}`, '_blank')
-        }
+    const openUrl = (url, name) => {
+      window.open(`${url}/${name}`, "_blank");
+    };
 
-        return { icon, openUrl }
-    },
-}
+    return { icon, openUrl };
+  },
+};
 </script>
 
 <style lang="scss">
 .contact {
-    .contact-group {
-        &:nth-child(1) {
-            margin: 50px 50px 0px;
-            padding: 30px 15px 0px;
-            border-top: 1px solid;
-            .box-1 {
-                div:nth-child(1) {
-                    opacity: 1;
-                }
-            }
-        }
-        margin: 0px 50px;
-        padding: 0px 15px 15px;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: nowrap;
-        justify-content: space-between;
-        .box-1,
-        .box-2 {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-        }
-        .box-1 {
-            div {
-                opacity: 0.5;
-            }
-        }
-        .box-2 {
-            text-align: center;
-            flex-direction: row;
-            align-items: flex-end;
-            .icon {
-                margin-right: 15px;
-                img {
-                    width: 40px;
-                }
-            }
-        }
-        .box-3 {
-            display: flex;
-            flex-direction: row;
-            align-items: flex-end;
-            justify-content: flex-end;
-            img {
-                cursor: pointer;
-                margin-right: 20px;
-                opacity: 0.5;
-            }
-        }
+  .contact-group {
+    margin: 50px 50px 50px;
+    padding: 30px 15px 0px;
+    border-top: 1px solid;
+    .address {
+      margin-bottom: 15px;
+      font-size: 14px;
+      div:nth-child(1) {
+        opacity: 1;
+        font-family: "Roboto-Bold";
+      }
+      div {
+        opacity: 0.5;
+        font-family: "Roboto-Medium";
+      }
     }
+    .google-culture {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 15px;
+      font-size: 14px;
+      font-family: "Prompt-Medium";
+      .icon {
+        margin-right: 15px;
+      }
+    }
+    .icon-social {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 15px;
+      img {
+        cursor: pointer;
+        margin-right: 10px;
+        opacity: 0.5;
+      }
+    }
+  }
 }
 .spaceline {
-    height: 100px;
-    border-top: 1px solid rgba(100, 100, 100, 0.2);
-}
-@media screen and (max-width: 768px) {
-    .content {
-        .vego-group .vego {
-            .vego-main,
-            .vego-flip {
-                font-size: calc(100px * 0.5);
-            }
-        }
-        .description {
-            p {
-                font-size: calc(18px * 0.5);
-            }
-            .customButton {
-                font-size: calc(18px * 0.5);
-            }
-        }
-        .contact .contact-group {
-            font-size: calc(16px * 0.5);
-            .box-3 img {
-                width: calc(40px * 0.5);
-            }
-        }
-    }
+  height: 50px;
+  width: 100%;
+  border-top: 1px solid rgba(100, 100, 100, 0.2);
 }
 </style>
--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,157 +1,192 @@
 <template>
-    <div class="footer">
-        <div class="overlay"></div>
-        <img
-            class="footer-bg"
-            :src="require('../../assets/images/home/footer/ve.png')"
-        />
-        <div class="content">
-            <div class="vego-group">
-                <div class="vego">
-                    <div class="vego-main">VEGO</div>
-                    <div class="vego-flip">VEGO</div>
-                </div>
-                <div class="description">
-                    <p>
-                        {{ $t('footer_description1') }} <br />
-                        {{ $t('footer_description2') }} <br />
-                        {{ $t('footer_description3') }}
-                    </p>
-                    <div class="button">
-                        <button type="button" class="btn customButton">
-                            <b>Let's Go</b>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <FooterContacts />
+  <div class="footer">
+    <div class="overlay"></div>
+    <img
+      class="footer-bg"
+      :src="require('../../assets/images/home/footer/ve.png')"
+    />
+    <div class="content">
+      <div class="vego-group">
+        <div class="vego">
+          <div class="vego-main">VEGO</div>
+          <div class="vego-flip">VEGO</div>
         </div>
+        <div class="description">
+          <p>
+            {{ $t("footer_description1") }} <br />
+            {{ $t("footer_description2") }} <br />
+            {{ $t("footer_description3") }}
+          </p>
+          <div class="button">
+            <button type="button" class="btn customButton">
+              <b>App Store</b>
+            </button>
+          </div>
+        </div>
+        <div class="socials">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <FooterContacts />
     </div>
+  </div>
 </template>
 
 <script>
-import FooterContacts from './footer-contact'
+import FooterContacts from "./footer-contact";
 export default {
-    components: {
-        FooterContacts,
-    },
-}
+  components: {
+    FooterContacts,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .footer {
-    position: relative;
-    .overlay {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background: linear-gradient(359deg, #030024 20%, rgba(0, 0, 0, 0));
-        z-index: 2;
+  position: relative;
+  background-image: url("../../assets/images/home/footer/ve.png");
+  //   background-size: 100%;
+  //   background-position: top;
+  //   background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: #030124;
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(359deg, #030024 20%, rgba(0, 0, 0, 0));
+    z-index: 2;
+  }
+  .footer-bg {
+    width: 100%;
+    z-index: 1;
+  }
+  .content {
+    // margin-top: 250px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 3;
+    color: white;
+    width: 100%;
+    .vego-group {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      transform: translateX(-10%);
+      line-height: 0.8;
+      flex-wrap: nowrap;
+      .vego {
+        display: flex;
+        flex-direction: column;
+        margin-right: 15px;
+        .vego-main {
+          font-size: 100px;
+          font-family: "Kanit-Black";
+          text-shadow: 0 0 12px #b51bff;
+        }
+        .vego-flip {
+          font-size: 100px;
+          font-family: "Kanit-Black";
+          transform: scaleY(-1);
+          background: -webkit-linear-gradient(to top, #ffffff4d, #00000000 55%);
+          background: -moz-linear-gradient(to top, #ffffff4d, #00000000 55%);
+          background: linear-gradient(to top, #ffffff4d, #00000000 55%);
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+        }
+      }
     }
+    .description {
+      font-family: "Prompt-Regular";
+      padding: 15px;
+      line-height: 1;
+      margin-right: 15px;
+      .customButton {
+        font-family: "Kanit-SemiBold";
+        color: #6268e9;
+        background-color: transparent;
+        font-size: 18px;
+        border-radius: 20px;
+        // width: 200px;
+        border: 2px solid #6268e9;
+        &:hover {
+          background-color: #6268e9;
+          color: #fff;
+        }
+        b {
+          padding: 0px 30px;
+        }
+      }
+    }
+    .socials {
+      display: flex;
+      div {
+        width: 100px;
+        height: 100px;
+        background-color: #fff;
+        &:nth-child(1) {
+          margin-right: 10px;
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 768px) {
     .footer-bg {
-        width: 100%;
-        z-index: 1;
+      display: none;
     }
     .content {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        z-index: 3;
-        color: white;
-        width: 100%;
-        .vego-group {
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-end;
-            transform: translateX(-10%);
-            line-height: 0.8;
-            flex-wrap: nowrap;
-            .vego {
-                display: flex;
-                flex-direction: column;
-                .vego-main {
-                    font-size: 100px;
-                    font-family: 'Kanit-Black';
-                    text-shadow: 0 0 12px #b51bff;
-                }
-                .vego-flip {
-                    font-size: 100px;
-                    font-family: 'Kanit-Black';
-                    transform: scaleY(-1);
-                    background: -webkit-linear-gradient(
-                        to top,
-                        #ffffff4d,
-                        #00000000 55%
-                    );
-                    background: -moz-linear-gradient(
-                        to top,
-                        #ffffff4d,
-                        #00000000 55%
-                    );
-                    background: linear-gradient(
-                        to top,
-                        #ffffff4d,
-                        #00000000 55%
-                    );
-                    -webkit-background-clip: text;
-                    -webkit-text-fill-color: transparent;
-                }
-            }
+      position: relative;
+      padding-top: 250px;
+      .vego-group .vego {
+        .vego-main,
+        .vego-flip {
+          font-size: calc(100px * 0.5);
+        }
+      }
+      .description {
+        p {
+          font-size: calc(18px * 0.5);
+        }
+        .customButton {
+          font-size: calc(18px * 0.5);
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 425px) {
+    .footer-bg {
+      display: none;
+    }
+    .content {
+      position: relative;
+      padding-top: 250px;
+      .vego-group {
+        flex-direction: column;
+        align-items: center;
+        transform: translateX(0%);
+        .vego {
+          .vego-main,
+          .vego-flip {
+            font-size: calc(70px * 0.5);
+          }
         }
         .description {
-            font-family: 'Kanit-Light';
-            padding: 15px;
-            line-height: 1;
+          margin-right: 0px;
+          p {
+            font-size: calc(18px * 0.5);
+          }
+          div.button {
+            text-align: center;
             .customButton {
-                font-family: 'Kanit-Medium';
-                color: #6268e9;
-                background-color: transparent;
-                font-size: 18px;
-                border-radius: 20px;
-                width: 200px;
-                border: 2px solid #6268e9;
-                &:hover {
-                    background-color: #6268e9;
-                    color: #fff;
-                }
+              font-size: calc(18px * 0.5);
             }
+          }
         }
+      }
     }
-    @media screen and (max-width: 768px) {
-        .content {
-            .vego-group .vego {
-                .vego-main,
-                .vego-flip {
-                    font-size: calc(100px * 0.5);
-                }
-            }
-            .description {
-                p {
-                    font-size: calc(18px * 0.5);
-                }
-                .customButton {
-                    font-size: calc(18px * 0.5);
-                }
-            }
-        }
-    }
-    @media screen and (max-width: 426px) {
-        .content {
-            .vego-group .vego {
-                .vego-main,
-                .vego-flip {
-                    font-size: calc(70px * 0.5);
-                }
-            }
-            .description {
-                p {
-                    font-size: calc(18px * 0.5);
-                }
-                .customButton {
-                    font-size: calc(18px * 0.5);
-                }
-            }
-        }
-    }
+  }
 }
 </style>
