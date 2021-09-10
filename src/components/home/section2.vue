3a1bf41031c74c57b7601297b1344eaa3c8d0bc0
srawit <srawit2540@gmail.com>
2021-09-10 19:47:05 +0700

    .

--- src/components/home/section2.vue
+++ src/components/home/section2.vue
@@ -1,42 +1,44 @@
 <template>
-  <div class="section2" :style="section2Height">
+  <div class="section2">
     <div class="container">
       <div class="title">
         {{ $t("section2_title") }}
       </div>
     </div>
 
-    <div class="content" :style="section2Height">
+    <div class="content">
       <div v-for="i in 5" :key="i" class="star-group">
         <div class="blur"></div>
         <div class="star"></div>
       </div>
-      <!-- <img
+      <img
         :src="require('@/assets/images/home/section2/Group 654.svg')"
         class="map"
-      /> -->
+      />
       <div class="row">
-        <div class="left-top col-6">
-          <p>
-            {{ $t("section2_leftTop") }}
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 812.png')"
-            class="logo"
-          />
+        <div class="left-top col-6" :style="height">
+          <div class="logo">
+            <img
+              :src="require('@/assets/images/home/section2/Group 812.png')"
+            />
+            <p>
+              {{ $t("section2_leftTop") }}
+            </p>
+          </div>
           <img
             :src="require('@/assets/images/home/section2/Path 61.svg')"
             class="path"
           />
         </div>
-        <div class="right-top col-6">
-          <p>
-            {{ $t("section2_rightTop") }}
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 809.png')"
-            class="logo"
-          />
+        <div class="right-top col-6" :style="height">
+          <div class="logo">
+            <img
+              :src="require('@/assets/images/home/section2/Group 809.png')"
+            />
+            <p>
+              {{ $t("section2_rightTop") }}
+            </p>
+          </div>
           <img
             :src="require('@/assets/images/home/section2/Path 50.svg')"
             class="path"
@@ -45,31 +47,33 @@
       </div>
 
       <div class="row">
-        <div class="left-bottom col-6">
-          <p>
-            {{ $t("section2_leftBottom") }}
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 811.png')"
-            class="logo"
-          />
+        <div class="left-bottom col-6" :style="height">
           <img
             :src="require('@/assets/images/home/section2/Path 124.svg')"
             class="path"
           />
+          <div class="logo">
+            <img
+              :src="require('@/assets/images/home/section2/Group 811 copy.png')"
+            />
+            <p>
+              {{ $t("section2_leftBottom") }}
+            </p>
+          </div>
         </div>
-        <div class="right-bottom col-6">
-          <p>
-            {{ $t("section2_rightBottom") }}
-          </p>
-          <img
-            :src="require('@/assets/images/home/section2/Group 810.png')"
-            class="logo"
-          />
+        <div class="right-bottom col-6" :style="height">
           <img
             :src="require('@/assets/images/home/section2/Path 420.svg')"
             class="path"
           />
+          <div class="logo">
+            <img
+              :src="require('@/assets/images/home/section2/Group 810 copy.png')"
+            />
+            <p>
+              {{ $t("section2_rightBottom") }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -82,13 +86,16 @@ import { onBeforeMount, computed } from "vue";
 export default {
   setup() {
     var section2Height = "min-height: 100vh;";
+    var height = "height: 500px;";
 
     onBeforeMount(() => {
       section2Height = `min-height: ${window.innerWidth / 1.5}px;`;
+      height = `height: ${window.innerWidth / 2.5}px`;
     });
 
     return {
       section2Height: computed(() => section2Height),
+      height: computed(() => height),
     };
   },
 };
